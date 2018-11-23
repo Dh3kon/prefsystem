f2fbf86295bed09ad54e3d42634c8923531cef1d
Kleber Silva <kb-max@hotmail.com>
2018-11-23 21:03:07 -0200

    Commit inicial

---
+++ src/main/java/com/myproject/pref/prefsystem/PrefSystemApplication.java
@@ -0,0 +1,12 @@
+package com.myproject.pref.prefsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrefSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myproject/pref/prefsystem/PrefSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myproject.pref.prefsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrefSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
